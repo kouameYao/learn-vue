d64e8e6cb60a83681fe0c9dcc41a727249b76852
kouameYao <yaojean0857@gmail.com>
2021-08-01 11:52:13 +0000

    MAJ

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="jumbotron">
+   <h3> {{ title }} </h3>
+   <p> {{ subTitle }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: String,
+    subTitle: String
+  }
+}
+</script>
